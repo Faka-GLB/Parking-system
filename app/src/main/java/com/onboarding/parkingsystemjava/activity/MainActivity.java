7203b097258d0dd5da47d0c8f28be8be1ac10d56
Facundo Martinez <martinez.facundo@globant.com>
2021-06-07 11:27:39 -0300

    branch para pantalla de inicio de app
    agregado a pantalla principal el título y botón de seteo
    
    modificado noombre de strings a ingles y agregado styles.xml con estilo de titulo y dimens.xml con dimen de separacion en layout
    
    addressed various PR notes, TODO: change layout
    
    forgot to add files to previous commit
    
    changed layout type and app name string
    
    renamed packages and some references from parkingSystemJava to estacionAR
    
    changed app_name string first letter to lower case to mantain consistency
    
    comments fix
    
    fixed new comments about title and code format
    
    fix in textview id

--- app/src/main/java/com/onboarding/parkingsystemjava/activity/MainActivity.java
+++ app/src/main/java/com/onboarding/parkingsystemjava/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.onboarding.parkingsystemjava;
+package com.onboarding.parkingsystemjava.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.onboarding.parkingsystemjava.R;
 
 public class MainActivity extends AppCompatActivity {
 
